85f24fd2ef52a7937f17e7733891ac4c21c72db7
Kim HyunWoo <stories2@naver.com>
2024-03-31 16:09:53 +0900

    Integrating test

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const createWindow = () => {
   win.loadFile("index.html");
   win.webContents.openDevTools();
   win.setAlwaysOnTop(true);
+  win.setResizable(false);
 };
 
 app.whenReady().then(() => {
