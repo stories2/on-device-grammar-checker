51bdd46a7ecbe2494a71b6f6c3dc2f92a202317e
Kim HyunWoo <stories2@naver.com>
2024-03-31 15:41:01 +0900

    Basic setup

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 600,
+    icon: path.join(__dirname, "images/icon@1x.png"),
+  });
+
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
+  win.setAlwaysOnTop(true);
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
